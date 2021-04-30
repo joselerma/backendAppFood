f22223aefcc1571f71ef395e024a6bf28fbd89ac
Jose Lerma <pepelerma.aguilera@gmail.com>
2021-04-30 15:42:30 -0500

    Arreglos a conexion db

--- src/db.js
+++ src/db.js
@@ -2,13 +2,14 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_DB, DATABASE_URL } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_DB } = process.env;
 
 const sequelize = new Sequelize(
-  DATABASE_URL
-    ? DATABASE_URL
-    : `${DB_USER}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DB}`,
+  process.env.DATABASE_URL ||
+    `${DB_USER}://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_DB}`,
   {
+    dialect: "postgres",
+    protocol: "postgres",
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   }
