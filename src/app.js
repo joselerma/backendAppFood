c9e99f20c74ffab01e53aea22cc659c4d38f0f94
Jose Lerma <pepelerma.aguilera@gmail.com>
2021-05-02 12:19:29 -0500

    Permitiendo acceso a netlify

--- src/app.js
+++ src/app.js
@@ -5,6 +5,8 @@ const morgan = require("morgan");
 const routes = require("./routes/index.js");
 const { conn } = require("./db.js");
 const { Recipe, Diets } = require("./db.js");
+require("dotenv").config();
+const { FRONT_END } = process.env;
 
 const server = express();
 
@@ -15,7 +17,7 @@ server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
 server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Origin", `${FRONT_END}`); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
